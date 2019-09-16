561ea0f4a5aa1179fc5f8ffea59638dc9306c8ac
Ravindra <srbharathee@yahoo.com.sg>
2019-09-16 14:45:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tf_utils',
-      version='0.1',
+      version='0.11',
       description='utilities for tf2',
       url='https://github.com/srbharathee/tf_utils',
       author='srbharathee',
